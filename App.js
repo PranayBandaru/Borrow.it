c6c98a1d26514e74e1ed6128652884ede3ce6e34
PranayBandaru <pranay18539@mechyd.ac.in>
2021-06-05 00:31:00 +0530

    first

--- App.js
+++ App.js
@@ -2,8 +2,9 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreen from './app/screens/HomeScreen';
+import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
-  return <HomeScreen />
+  return <WelcomeScreen />
 }
 
---
+++ app/screens/WelcomeScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Image, StyleSheet, TextInput, View, Text, SafeAreaView, TouchableOpacity } from 'react-native';
+
+function HomeScreen(props) {
+    return (
+        <Image source={require("../assets/borrowit-logo.png")} />
+    );
+}
