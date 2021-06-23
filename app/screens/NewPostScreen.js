b919b17e68219cc45b93b429a19957d4aabb66fa
ShanmukhSreeenivas <shanmukhasreenivas12@gmail.com>
2021-06-23 15:28:53 +0530

    Postrepair

--- app/screens/NewPostScreen.js
+++ app/screens/NewPostScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { ScrollView } from "react-native-gesture-handler";
 
 import colors from "../config/colors";
@@ -9,35 +9,44 @@ import PickerField from "../components/forms/PickerField";
 import SubmitButton from "../components/forms/SubmitButton";
 import ImageField from "../components/forms/ImageIField";
 import * as Yup from "yup";
+import AppButton from '../components/AppButton';
 
 
 
-const validationSchema = Yup.object().shape({
-  images: Yup.array().min(1, "You must insert an image."),
-  title: Yup.string().required().min(1).label("Title"),
-  price: Yup.number().required().label("Price for a day"),
-  description: Yup.string().required().label("Description"),
-  category: Yup.string().required().nullable().label("Category"),
-});
 
-const categories = [
-  { label: "Sports", id: 1 },
-  { label: "Music Instruments", id: 2 },
-  { label: "Books", id: 3 },
-  { label: "Stationery", id: 4 },
-  { label: "Electronic Devices", id: 5 },
-  { label: "Clothing", id: 6 },
-  { label: "Accessories", id: 7 },
-  { label: "Other", id: 8 },
-];
+export default class  AppListingFormScreen extends Component {
+ 
+  constructor(props){
+    super(props);
+    this.onSubmit == this.onSubmit.bind(this);    
+  }
+  
 
+  render(){
+    const validationSchema = Yup.object().shape({
+      images: Yup.array().min(1, "You must insert an image."),
+      title: Yup.string().required().min(1).label("Title"),
+      price: Yup.number().required().label("Price for a day"),
+      description: Yup.string().required().label("Description"),
+      category: Yup.string().required().nullable().label("Category"),
+    });
+    
+    const categories = [
+      { label: "Sports", id: 1 },
+      { label: "Music Instruments", id: 2 },
+      { label: "Books", id: 3 },
+      { label: "Stationery", id: 4 },
+      { label: "Electronic Devices", id: 5 },
+      { label: "Clothing", id: 6 },
+      { label: "Accessories", id: 7 },
+      { label: "Other", id: 8 },
+    ];
 
-function AppListingFormScreen(props) {
   return (
     <ScrollView
       style={{ flex: 1, backgroundColor: colors.light }}
       showsVerticalScrollIndicator={false}
-    >
+      >
       <AppScreen>
         <FromContainer
           initialValues={{
@@ -48,7 +57,7 @@ function AppListingFormScreen(props) {
             description: "",
           }}
           validationSchema={validationSchema}
-        //onSubmit={onSubmit}
+          onSubmit={this.onSubmit}
         >
           <>
             <ImageField name="images" />
@@ -86,12 +95,15 @@ function AppListingFormScreen(props) {
               keyboardType="default"
               placeholder="Description"
             />
-            <SubmitButton title="Post" color={colors.primary} />
+            {/*<SubmitButton title="Post" color={colors.primary} />*/}
+            <AppButton title="POST" color="#0c7171" onPress={this.onSubmit} /> 
           </>
         </FromContainer>
       </AppScreen>
     </ScrollView>
   );
 }
-
-export default AppListingFormScreen;
+onSubmit = () => {
+  this.props.navigation.navigate('Home');
+}
+}
